6a3439596b85fd71e82a72438a9c63b12b5db477
angelbeltre-006 <angel.beltre@upr.edu>
2025-09-04 11:21:08 -0400

    TASK 3- DONE(AB) Changed ball color from light grey to purple.

--- PA0.py
+++ PA0.py
@@ -121,7 +121,7 @@ while True:
     light_grey = pygame.Color('grey83')
     red = pygame.Color('red')
     screen.fill(bg_color)  # Clear screen with background color
-    pygame.draw.rect(screen, light_grey, player)  # Draw player paddle
+    pygame.draw.rect(screen, morado, player)  # Draw player paddle
     # TODO Task 3: Change the Ball Color
     pygame.draw.ellipse(screen, light_grey, ball)  # Draw ball
     player_text = basic_font.render(f'{score}', False, light_grey)  # Render player score
